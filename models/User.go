f4de5d2a94ab65729ada307207d687ce36bc25f3
Yoga Cahya R <yogacahya08@gmail.com>
2022-09-25 16:22:53 +0700

    refactor : add constraint for id and email, add BeforeSave function

--- models/User.go
+++ models/User.go
@@ -1,26 +1,36 @@
 package models
 
 import (
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"time"
 )
 
 type User struct {
-	ID        uint
+	ID        uint `gorm:"primary_key;auto_increment"`
 	Name      string
-	Email     string
+	Email     string `gorm:"unique"`
 	Password  string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	Photos    []Photo `gorm:"OnDelete:CASCADE;"`
 }
 
-func (u *User) CreateUser(db *gorm.DB) (*User, error) {
+func (u *User) SaveUser(db *gorm.DB) (*User, error) {
 	err := db.Create(&u).Error
 	if err != nil {
 		return nil, err
 	}
 	return u, nil
+}
+
+func (u *User) BeforeSave(db *gorm.DB) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
 }
 
 func (u *User) GetUser(db *gorm.DB, uid uint) (*User, error) {
