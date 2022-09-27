fb8557bf69b50fb0a2005c86e5242fc89cb6fa47
Yoga Cahya R <yogacahya08@gmail.com>
2022-09-27 22:25:56 +0700

    feat : add update and delete user

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"golang.org/x/crypto/bcrypt"
@@ -18,6 +19,11 @@ type RegisInput struct {
 type LoginInput struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
+}
+
+type UpdateInput struct {
+	Name  string `json:"name" validate:"required"`
+	Email string `json:"email" validate:"required,email"`
 }
 
 func (s *Server) CreateUser(c *gin.Context) {
@@ -77,4 +83,73 @@ func (s *Server) Login(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"token": tokenString})
+}
+
+func (s *Server) UpdateUser(c *gin.Context) {
+	var input UpdateInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	validate := validator.New()
+	err := validate.Struct(input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "input does not match the criteria"})
+		return
+	}
+	userId, err := helpers.GetUserIdFromToken(c.Request)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	userIdFromUrl := c.Param("userId")
+
+	if userIdFromUrl != fmt.Sprint(userId) {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	user := models.User{}
+	_, err = user.GetUser(s.DB, userId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	user.Name = input.Name
+	user.Email = input.Email
+
+	if _, err = user.UpdateUser(s.DB, userId); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+func (s *Server) DeleteUser(c *gin.Context) {
+	userId, err := helpers.GetUserIdFromToken(c.Request)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	userIdFromUrl := c.Param("userId")
+
+	if userIdFromUrl != fmt.Sprint(userId) {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	user := models.User{}
+
+	_, err = user.DeleteUser(s.DB, userId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "success"})
 }
